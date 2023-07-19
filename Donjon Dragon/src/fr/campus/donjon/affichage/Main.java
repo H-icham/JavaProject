1c5f1e0ff9297e1a95070b45cbe060941b7c5ed0
Hicham Bouzelmad <hicham.bouzelmad@le-campus-numerique.fr>
2023-07-19 13:43:31 +0200

    itération 2

---
+++ Donjon Dragon/src/fr/campus/donjon/affichage/Main.java
@@ -0,0 +1,11 @@
+package fr.campus.donjon.affichage;
+
+import fr.campus.donjon.Jeu.Game;
+import fr.campus.donjon.avatars.Personnage;
+
+public class Main {
+    public static void main(String[] args) {
+        Game jeux = new Game();
+        jeux.enterMenuChoice();
+    }
+}
